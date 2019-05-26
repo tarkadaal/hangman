577c0de81b95af0193b02b69584575017f2b528f
Dan Jackson <danjacksonis@googlemail.com>
2019-05-26 12:35:52 +0200

    Added basics of game logic

--- core/hangman.py
+++ core/hangman.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 TARGET_WORDS =[
     "3dhubs", "marvin", "print", "filament", "order", "layer"
@@ -13,5 +14,43 @@ class HangmanState():
         self.current_discovered = [None for x in range(len(self.target_word))]
         self.lives_left = LIVES
 
+
 def start_game():
     return HangmanState()
+
+
+def take_turn(state, guess):
+    if state is None:
+        raise ValueError("The 'state' parameter cannot be None")
+
+    if guess is None:
+        raise ValueError("The 'guess' parameter cannot be None")
+
+    new_state = copy.copy(state)
+    if guess in state.target_word:
+        new_state.current_discovered = _calculate_new_discovered(
+            state.target_word,
+            state.current_discovered,
+            guess
+        )
+        new_state.is_finished = None not in new_state.current_discovered
+    else:
+        new_state.lives_left -= 1
+    
+    return new_state
+
+
+def _calculate_new_discovered(target, current_discovered, guess, modifier=0):
+    index = target.find(guess)
+    new_discovered = copy.copy(current_discovered)
+    new_discovered[index + modifier] = guess
+
+    rest = target[index+1:]
+    if guess in rest:
+        new_discovered = _calculate_new_discovered(
+            rest,
+            new_discovered,
+            guess,
+            index + 1
+        )
+    return new_discovered
--- core/test_hangman.py
+++ core/test_hangman.py
@@ -1,6 +1,7 @@
 import unittest
 import core.hangman as hangman
 
+
 class HangmanStartGameTests(unittest.TestCase):
     def setUp(self):
         self.result = hangman.start_game()
@@ -28,6 +29,72 @@ class HangmanStartGameTests(unittest.TestCase):
 
     def test_game_has_lives_left(self):
         self.assertEqual(5, self.result.lives_left)
+
+
+class HangmanTakeTurnTests(unittest.TestCase):
+    def test_state_cannot_be_none(self):
+        with self.assertRaises(ValueError):
+            hangman.take_turn(None, "m")
+
+    def test_guess_cannot_be_none(self):
+        with self.assertRaises(ValueError):
+            hangman.take_turn(hangman.start_game(), None)
+
+    #pass in correct letter you don't have
+    def test_pass_in_correct_letter_we_dont_have(self):
+        state = hangman.HangmanState(["monitor"])
+        result = hangman.take_turn(state, "o")
+        self.assertEqual(False, result.is_finished)
+        self.assertEqual(state.target_word, result.target_word)
+        self.assertEqual(
+            [None, "o", None, None, None, "o", None],
+            result.current_discovered
+        )
+        self.assertEqual(state.lives_left, result.lives_left)
+
+
+    #pass in correct letter you have
+    def test_pass_in_correct_letter_we_have(self):
+        state = hangman.HangmanState(["monitor"])
+        state.current_discovered = [None, "o", None, None, None, "o", None]
+        result = hangman.take_turn(state, "o")
+        self.assertEqual(False, result.is_finished)
+        self.assertEqual(state.target_word, result.target_word)
+        self.assertEqual(
+            [None, "o", None, None, None, "o", None],
+            result.current_discovered
+        )
+        self.assertEqual(state.lives_left, result.lives_left)
+
+    #pass in final correct letter
+    def test_pass_in_final_correct_letter(self):
+        state = hangman.HangmanState(["monitor"])
+        state.current_discovered = ["m", "o", "n", None, "t", "o", "r"]
+        result = hangman.take_turn(state, "i")
+        self.assertEqual(True, result.is_finished)
+        self.assertEqual(state.target_word, result.target_word)
+        self.assertEqual(
+            ["m", "o", "n", "i", "t", "o", "r"],
+            result.current_discovered
+        )
+        self.assertEqual(state.lives_left, result.lives_left)
+
+    #pass in incorrect letter
+    def test_guess_incorrect_letter(self):
+        state = hangman.HangmanState(["monitor"])
+        result = hangman.take_turn(state, "x")
+        self.assertEqual(False, result.is_finished)
+        self.assertEqual(state.target_word, result.target_word)
+        self.assertEqual(
+            state.current_discovered,
+            result.current_discovered
+        )
+        self.assertEqual(state.lives_left -1 , result.lives_left)
+
+    #pass in incorrect letter when you're on your last life
+    #pass in finished game
+    #pass in multi-character guess
+
 
 if __name__ == '__main__':
     unittest.main()
