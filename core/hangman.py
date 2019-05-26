61c673ff9dcc4097dedcb36a13f9b4b5c128f87a
Dan Jackson <danjacksonis@googlemail.com>
2019-05-26 16:47:42 +0200

    Added string formatting. Logic should now be correct.

--- core/hangman.py
+++ core/hangman.py
@@ -7,37 +7,45 @@ TARGET_WORDS =[
 LIVES = 5
 
 class HangmanState():
-    def __init__(self, target_words=TARGET_WORDS):
+    def __init__(self, target_words):
         self.is_finished = False
         target = random.randint(0, len(target_words) - 1)
         self.target_word = target_words[target]
-        self.current_discovered = [None for x in range(len(self.target_word))]
+        self.current_known = [None for x in range(len(self.target_word))]
         self.lives_left = LIVES
 
     @property
     def has_won(self):
-        return self.is_finished and None not in self.current_discovered
+        return self.is_finished and None not in self.current_known
 
 
-def start_game():
-    return HangmanState()
+def start_game(possible_words=None):
+    words = possible_words if possible_words else TARGET_WORDS
+    return HangmanState(words)
 
 
 def take_turn(state, guess):
     if state is None:
-        raise ValueError("The 'state' parameter cannot be None")
+        raise ValueError("The 'state' parameter cannot be None.")
+
+    if state.is_finished:
+        raise ValueError(
+            "This game is finished, no further moves can be taken.")
 
     if guess is None:
-        raise ValueError("The 'guess' parameter cannot be None")
+        raise ValueError("The 'guess' parameter cannot be None.")
+
+    if not len(guess) == 1:
+        raise ValueError("The 'guess' parameter must be a single character.")
 
     new_state = copy.copy(state)
     if guess in state.target_word:
-        new_state.current_discovered = _calculate_new_discovered(
+        new_state.current_known = _calculate_new_known(
             state.target_word,
-            state.current_discovered,
+            state.current_known,
             guess
         )
-        new_state.is_finished = None not in new_state.current_discovered
+        new_state.is_finished = None not in new_state.current_known
     else:
         new_state.lives_left -= 1
         if new_state.lives_left == 0:
@@ -45,17 +53,25 @@ def take_turn(state, guess):
     return new_state
 
 
-def _calculate_new_discovered(target, current_discovered, guess, modifier=0):
+def _calculate_new_known(target, current_known, guess, modifier=0):
     index = target.find(guess)
-    new_discovered = copy.copy(current_discovered)
-    new_discovered[index + modifier] = guess
+    new_known = copy.copy(current_known)
+    new_known[index + modifier] = guess
 
     rest = target[index+1:]
     if guess in rest:
-        new_discovered = _calculate_new_discovered(
+        new_known = _calculate_new_known(
             rest,
-            new_discovered,
+            new_known,
             guess,
             index + 1
         )
-    return new_discovered
+    return new_known
+
+
+def format_current_known(known):
+    if not known:
+        raise ValueError("The 'known' parameter must be a valid hangman known state.")
+
+    santized = [x if x else "_" for x in known]
+    return "".join(santized)
--- core/test_hangman.py
+++ core/test_hangman.py
@@ -16,15 +16,15 @@ class HangmanStartGameTests(unittest.TestCase):
         self.assertIsNotNone(self.result.target_word)
         self.assertIn(self.result.target_word, hangman.TARGET_WORDS)
 
-    def test_game_has_current_discovered(self):
-        self.assertIsNotNone(self.result.current_discovered)
+    def test_game_has_current_known(self):
+        self.assertIsNotNone(self.result.current_known)
         self.assertTrue(
-            len(self.result.current_discovered), 
+            len(self.result.current_known), 
             len(self.result.target_word)
         )
         self.assertSequenceEqual(
             [None for x in range(len(self.result.target_word))],
-            self.result.current_discovered
+            self.result.current_known
         )
 
     def test_game_has_lives_left(self):
@@ -41,12 +41,21 @@ class HangmanTakeTurnTests(unittest.TestCase):
     def test_guess_cannot_be_none(self):
         with self.assertRaises(ValueError):
             hangman.take_turn(hangman.start_game(), None)
+    
+    def test_guess_cannot_be_multiple_characters(self):
+        with self.assertRaises(ValueError):
+            hangman.take_turn(hangman.start_game(), "hi")
 
-    def _test_take_turn_core(self,
+    def test_guess_cannot_be_empty_string(self):
+        with self.assertRaises(ValueError):
+            hangman.take_turn(hangman.start_game(), "")
+
+    def _test_take_turn_core(
+        self,
         state,
         guess,
         expected_is_finished,
-        expected_current_discovered,
+        expected_current_known,
         expected_lives_left,
         expected_has_won=False
     ):
@@ -54,13 +63,13 @@ class HangmanTakeTurnTests(unittest.TestCase):
         self.assertEqual(expected_is_finished, result.is_finished)
         self.assertEqual(state.target_word, result.target_word)
         self.assertEqual(
-            expected_current_discovered,
-            result.current_discovered
+            expected_current_known,
+            result.current_known
         )
         self.assertEqual(expected_lives_left, result.lives_left)
         self.assertEqual(expected_has_won, result.has_won)
 
-    #pass in correct letter you don't have
+
     def test_pass_in_correct_letter_we_dont_have(self):
         state = hangman.HangmanState(["monitor"])
         self._test_take_turn_core(
@@ -72,10 +81,9 @@ class HangmanTakeTurnTests(unittest.TestCase):
         )
 
 
-    #pass in correct letter you have
     def test_pass_in_correct_letter_we_have(self):
         state = hangman.HangmanState(["monitor"])
-        state.current_discovered = [None, "o", None, None, None, "o", None]
+        state.current_known = [None, "o", None, None, None, "o", None]
         self._test_take_turn_core(
             state,
             "o",
@@ -84,10 +92,9 @@ class HangmanTakeTurnTests(unittest.TestCase):
             state.lives_left
         )
 
-    #pass in final correct letter
     def test_pass_in_final_correct_letter(self):
         state = hangman.HangmanState(["monitor"])
-        state.current_discovered = ["m", "o", "n", None, "t", "o", "r"]
+        state.current_known = ["m", "o", "n", None, "t", "o", "r"]
         self._test_take_turn_core(
             state,
             "i",
@@ -97,18 +104,16 @@ class HangmanTakeTurnTests(unittest.TestCase):
             True
         )
 
-    #pass in incorrect letter
     def test_guess_incorrect_letter(self):
         state = hangman.HangmanState(["monitor"])
         self._test_take_turn_core(
             state,
             "x",
             False,
-            state.current_discovered,
+            state.current_known,
             state.lives_left - 1
         )
 
-    #pass in incorrect letter when you're on your last life
     def test_guess_incorrectly_and_lose_last_life(self):
         state = hangman.HangmanState(["monitor"])
         state.lives_left = 1
@@ -116,14 +121,48 @@ class HangmanTakeTurnTests(unittest.TestCase):
             state,
             "x",
             True,
-            state.current_discovered,
+            state.current_known,
             0
         )
 
-    #should store win/loss, not just finished
-    #pass in finished game
-    #pass in multi-character guess
+    def test_raises_if_game_is_finished(self):
+        state = hangman.HangmanState(["string"])
+        state.is_finished = True
+        state.current_known = list(state.target_word)
+        state.lives_left = 3
+        with self.assertRaises(ValueError):
+            self._test_take_turn_core(
+                state,
+                "s",
+                True,
+                state.current_known,
+                3,
+                True
+            )
 
+
+class HangmanStringFormatterTests(unittest.TestCase):
+    def test_raises_on_empty(self):
+        with self.assertRaises(ValueError):
+            hangman.format_current_known([])
+
+    def test_with_completely_unknown(self):
+        result = hangman.format_current_known(
+            [None for x in range(6)]
+        )
+        self.assertEqual("______", result)
+
+    def test_with_some_known_letters(self):
+        result = hangman.format_current_known(
+            ["p", None, "o", "n", None]
+        )
+        self.assertEqual("p_on_", result)
+
+    def test_with_all_known_letters(self):
+        result = hangman.format_current_known(
+            ["u", "t", "i", "l", "i", "t", "y"]
+        )
+        self.assertEqual("utility", result)
 
 if __name__ == '__main__':
     unittest.main()
