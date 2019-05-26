e7f3757d1eef991d39a0326f21c49c92f67d2725
Dan Jackson <danjacksonis@googlemail.com>
2019-05-26 11:12:05 +0200

    Added current_discovered and tests

--- core/hangman.py
+++ core/hangman.py
@@ -1,6 +1,15 @@
+import random
+
+TARGET_WORDS =[
+    "3dhubs", "marvin", "print", "filament", "order", "layer"
+]
+
 class HangmanState():
-    def __init__(self):
+    def __init__(self, target_words=TARGET_WORDS):
         self.is_finished = False
+        target = random.randint(0, len(target_words) - 1)
+        self.target_word = target_words[target]
+        self.current_discovered = [None for x in range(len(self.target_word))]
 
 def start_game():
     return HangmanState()
--- core/test_hangman.py
+++ core/test_hangman.py
@@ -1,7 +1,7 @@
 import unittest
 import core.hangman as hangman
 
-class HangmanTests(unittest.TestCase):
+class HangmanStartGameTests(unittest.TestCase):
     def test_can_start_game(self):
         result = hangman.start_game()
         self.assertIsNotNone(result)
@@ -9,6 +9,23 @@ class HangmanTests(unittest.TestCase):
     def test_game_starts_unfinished(self):
         result = hangman.start_game()
         self.assertFalse(result.is_finished)
+
+    def test_game_has_target_word(self):
+        result = hangman.start_game()
+        self.assertIsNotNone(result.target_word)
+        self.assertIn(result.target_word, hangman.TARGET_WORDS)
+
+    def test_game_has_current_discovered(self):
+        result = hangman.start_game()
+        self.assertIsNotNone(result.current_discovered)
+        self.assertTrue(
+            len(result.current_discovered), 
+            len(result.target_word)
+        )
+        self.assertSequenceEqual(
+            [None for x in range(len(result.target_word))],
+            result.current_discovered
+        )
 
 if __name__ == '__main__':
     unittest.main()
