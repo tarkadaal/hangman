53e54a568959cf94332e7cf39c05aa506bd69383
Dan Jackson <danjacksonis@googlemail.com>
2019-05-31 11:44:51 +0200

    HangmanStorage class now actually stores things.

--- core/hangman_storage.py
+++ core/hangman_storage.py
@@ -4,8 +4,18 @@ class HangmanStorage():
             raise ValueError("The 'buffer' parameter must be a valid stream.")
 
         self._buffer = buffer
+        self._buffer.seek(0)
         line = self._buffer.readline()
         self._high_score = 0 if line == "" else int(line)
 
     def get_high_score(self):
         return self._high_score
+
+    def update_high_score_if_lower_than(self, new_score):
+        is_new_score_higher = self._high_score < new_score
+        if is_new_score_higher:
+            self._buffer.seek(0)
+            self._buffer.write(str(new_score))
+            self._buffer.flush()
+            self._high_score = new_score
+        return is_new_score_higher
--- core/test_hangman_storage.py
+++ core/test_hangman_storage.py
@@ -3,25 +3,50 @@ import io
 from core.hangman_storage import HangmanStorage
 
 class HangmanStorageTests(unittest.TestCase):
-    def test_is_constructed_with_stream(self):
-        with io.StringIO("") as buf:
-            self.assertIsNotNone(HangmanStorage(buf))
+    def _initialise_sut_with(text):
+        def wrap(func):
+            def core(self):
+                with io.StringIO(text) as buf:
+                    sut = HangmanStorage(buf)
+                    func(self, sut)
+            return core
+        return wrap
 
     def test_raises_if_no_stream(self):
         with self.assertRaises(ValueError):
             HangmanStorage(None)
 
-    def test_empty_streams_default_to_zero(self):
-        with io.StringIO("") as buf:
-            sut = HangmanStorage(buf)
-            self.assertEqual(0, sut.get_high_score())
-
-    def test_score_is_read_from_stream(self):
-        with io.StringIO("4") as buf:
-            sut = HangmanStorage(buf)
-            self.assertEqual(4, sut.get_high_score())
-
     def test_raises_if_data_is_corrupt(self):
         with io.StringIO("corrupt data") as buf:
             with self.assertRaises(ValueError):
                 HangmanStorage(buf)
+
+    @_initialise_sut_with("")
+    def test_empty_streams_default_to_zero(self, sut):
+        self.assertEqual(0, sut.get_high_score())
+
+    @_initialise_sut_with("4")
+    def test_score_is_read_from_stream(self, sut):
+        self.assertEqual(4, sut.get_high_score())
+
+    @_initialise_sut_with("2")
+    def test_update_replaces_with_higher_score(self, sut):
+        sut.update_high_score_if_lower_than(3)
+        self.assertEqual(3, sut.get_high_score())
+
+    @_initialise_sut_with("2")
+    def test_update_does_not_replace_lower_score(self, sut):
+        sut.update_high_score_if_lower_than(1)
+        self.assertEqual(2, sut.get_high_score())
+
+    @_initialise_sut_with("2")
+    def test_update_returns_true_if_new_score_is_higher(self, sut):
+        result = sut.update_high_score_if_lower_than(4)
+        self.assertEqual(True, result)
+
+    def test_update_persists_changes(self):
+        with io.StringIO("2") as buf:
+            sut = HangmanStorage(buf)
+            sut.update_high_score_if_lower_than(3)
+            sut = HangmanStorage(buf)  # reinitialise to check peristence
+            self.assertEqual(3, sut.get_high_score())
