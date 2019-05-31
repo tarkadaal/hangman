61349ca4cb00e2ec12f75ae7d500a69b575c9c0a
Dan Jackson <danjacksonis@googlemail.com>
2019-05-31 12:29:50 +0200

    Refactored play_hangman for readability.

--- play_hangman.py
+++ play_hangman.py
@@ -3,6 +3,29 @@ from core.hangman_storage import HangmanStorage
 
 STORAGE = "./high_scores"
 
+
+def main():
+    intro()
+    state = game_loop()
+    ending(state)
+
+
+def intro():
+    print("Welcome to Hangman!")
+    high_score = read_high_score()
+    print(f"Try to beat the high score of {high_score} points!")
+
+
+def game_loop():
+    state = hangman.start_game()
+    while not state.is_finished:
+        print_state(state)
+        guess = prompt_for_guess()
+        state = hangman.take_turn(state, guess)
+        print_was_guess_correct(state)
+    return state
+
+
 def print_state(state):
     formatted_current_guess = hangman.format_current_known(state.current_known)
     print(f"Lives remaining: {state.lives_left}")
@@ -22,30 +45,27 @@ def print_was_guess_correct(state):
         print("You lose a life.")
 
 
-def main():
-    print("Welcome to Hangman!")
-    with open(STORAGE, "a+") as file:
-        storage = HangmanStorage(file)
-        high_score = storage.get_high_score()
-    print(f"Try to beat the high score of {high_score} points!")
-    
-    state = hangman.start_game()
-    while not state.is_finished:
-        print_state(state)
-        guess = prompt_for_guess()
-        state = hangman.take_turn(state, guess)
-        print_was_guess_correct(state)
-
+def ending(state):
     if state.has_won:
         print("Well done!")
         score = hangman.calculate_score(state)
         print(f"You scored {score} points!")
-        with open(STORAGE, "r+") as file:
-            storage = HangmanStorage(file)
-            if storage.update_high_score_if_lower_than(score):
-                print("That's a new high score!!")
+        if update_high_score(score):
+            print("That's a new high score!!")
     else:
         print("Sorry, better luck next time.")
+
+
+def read_high_score():
+    with open(STORAGE, "a+") as file:
+        storage = HangmanStorage(file)
+        return storage.get_high_score()
+
+
+def update_high_score(new_score):
+    with open(STORAGE, "r+") as file:
+        storage = HangmanStorage(file)
+        return storage.update_high_score_if_lower_than(new_score)
 
 
 if __name__ == "__main__":
