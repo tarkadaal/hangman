2a06ea15227d5a95fef32e883334257d3f954971
Dan Jackson <danjacksonis@googlemail.com>
2019-05-31 12:17:51 +0200

    Can now play hangman with high scores!

--- play_hangman.py
+++ play_hangman.py
@@ -1,4 +1,7 @@
 import core.hangman as hangman
+from core.hangman_storage import HangmanStorage
+
+STORAGE = "./high_scores"
 
 def print_state(state):
     formatted_current_guess = hangman.format_current_known(state.current_known)
@@ -21,6 +24,11 @@ def print_was_guess_correct(state):
 
 def main():
     print("Welcome to Hangman!")
+    with open(STORAGE, "a+") as file:
+        storage = HangmanStorage(file)
+        high_score = storage.get_high_score()
+    print(f"Try to beat the high score of {high_score} points!")
+    
     state = hangman.start_game()
     while not state.is_finished:
         print_state(state)
@@ -30,6 +38,12 @@ def main():
 
     if state.has_won:
         print("Well done!")
+        score = hangman.calculate_score(state)
+        print(f"You scored {score} points!")
+        with open(STORAGE, "r+") as file:
+            storage = HangmanStorage(file)
+            if storage.update_high_score_if_lower_than(score):
+                print("That's a new high score!!")
     else:
         print("Sorry, better luck next time.")
 
