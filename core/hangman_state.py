ce535a8ea0eadcbb72ac7777479bfa2d11660c37
Dan Jackson <danjacksonis@googlemail.com>
2019-06-15 15:16:30 +0200

    Added comments to hangman_state, the main data structure.

--- core/hangman_state.py
+++ core/hangman_state.py
@@ -10,10 +10,22 @@ class HangmanState():
             lives_left,
             was_last_guess_correct
     ):
+        # Whether the game is still running or has finished
         self.is_finished = is_finished
+
+        # Which word the player is trying to guess
         self.target_word = target_word
+
+        # This is a list, containing the player's current knowledge of 
+        # the word. It starts out as containing one None for each letter
+        # in the target word, which get replaced with letters once the
+        # player guesses them correctly.
         self.current_known = current_known
+
+        # Number of lives remaining.
         self.lives_left = lives_left
+
+        # Was the player's last guess correct?
         self.was_last_guess_correct = was_last_guess_correct
 
     @staticmethod
